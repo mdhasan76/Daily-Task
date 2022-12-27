68ad6c566261dbcc34b99add515a4b9f44e7bd0b
Md Hasan <mdhasan8064@gmail.com>
2022-12-27 23:16:42 +0600

    register page

--- src/page/login/Login.js
+++ src/page/login/Login.js
@@ -6,19 +6,19 @@ const Login = () => {
 <div class="min-h-screen flex justify-center items-center bg-white">
   <div class="p-10 border-[1px] -mt-10 border-slate-200 rounded-md flex flex-col items-center space-y-3">
     <div class="py-8">
-      <img alt='' width="30" class="-mt-10" src="https://www.paypalobjects.com/images/shared/momgram@2x.png" />
+      <p className='font-bold text-3xl text-center'>Login</p>
     </div>
     <input class="p-3 border-[1px] border-slate-500 rounded-sm w-80" placeholder="E-Mail" />
     <div class="flex flex-col space-y-1">
       <input class="p-3 border-[1px] border-slate-500 rounded-sm w-80" placeholder="Password" />
-      <p class="font-semibold ">First at Daily task? <Link to="/register" className='hover:text-[#0070ba] hover:border-b'>Register now </Link> </p>
+      <p class="font-semibold text-xs">First at Daily task? <Link to="/register" className='hover:text-[#0070ba] hover:border-b'>Register now </Link> </p>
     </div>
     <div class="flex flex-col space-y-5 w-full">
-      <button class="w-full bg-[#0070ba] rounded-3xl p-3 text-white font-bold transition duration-200 hover:bg-[#003087]">Log in</button>
+      <button class="w-full mt-3 bg-[#0070ba] rounded-3xl p-3 text-white font-bold transition duration-200 hover:bg-[#003087]">Log in</button>
       <div class="flex items-center justify-center border-t-[1px] border-t-slate-300 w-full relative">
         <div class="-mt-1 font-bod bg-white px-5 absolute">Or</div>
       </div>
-      <button class="w-full border-blue-900 hover:border-[#003087] hover:border-[2px] border-[1px] rounded-3xl p-3 text-[#0070ba] font-bold transition duration-200">Sign Up</button>
+      <button class="w-full border-blue-900 hover:border-[#003087] hover:border-[2px] border-[1px] rounded-3xl p-3 text-[#0070ba] font-bold transition duration-200">Continew with google</button>
     </div>
   </div>
 </div>
--- src/page/register/Register.js
+++ src/page/register/Register.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            register
-        </div>
+        <div class="min-h-screen flex justify-center items-center bg-white">
+  <div class="p-10 border-[1px] -mt-10 border-slate-200 rounded-md flex flex-col items-center space-y-3">
+    <div class="py-8">
+      <p className='font-bold text-3xl text-center'>Create Account</p>
+    </div>
+    <input type="text" class="p-3 border-[1px] border-slate-500 rounded-sm w-80" placeholder="Full Name" />
+    <input type="email" class="p-3 border-[1px] border-slate-500 rounded-sm w-80" placeholder="E-Mail" />
+    <div class="flex flex-col space-y-1">
+      <input type="password" class="p-3 border-[1px] border-slate-500 rounded-sm w-80" placeholder="Password" />
+      <p class="font-semibold text-xs">Already have account? <Link to="/login" className='hover:text-[#0070ba] hover:border-b'>login now </Link> </p>
+    </div>
+    <div class="flex flex-col space-y-5 w-full">
+      <button class="w-full mt-3 bg-[#0070ba] rounded-3xl p-3 text-white font-bold transition duration-200 hover:bg-[#003087]">Register</button>
+    </div>
+  </div>
+</div>
     );
 };
 
